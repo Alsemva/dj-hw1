ea8deeaa0dd66d7532fa6872abf6dce057f348ad
Alexey Shorokhov <alsemva@yandex.ru>
2023-02-13 12:17:07 +0600

    HW mandatory part

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.template.defaultfilters import slugify
 
 
 class Command(BaseCommand):
@@ -11,7 +12,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
+            print(phones)
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            new_phone = Phone(
+                id=int(phone['id']),
+                name=phone['name'],
+                image=phone['image'],
+                price=phone['price'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name']))
+            new_phone.save()
+        return print('True!!!!!!!!!!!!!!!!!!')
+    # def handle(self, *args, **options):
+    #     with open('phones.csv', 'r') as csvfile:
+    #
+    #         phone_reader = csv.reader(csvfile, delimiter=';')
+    #         # пропускаем заголовок
+    #         next(phone_reader)
+    #
+    #         for line in phone_reader:
+    #             new_phone = Phone.objects.create(
+    #                 id=int(line[0]),
+    #                 name=line[1],
+    #                 image=line[2],
+    #                 price=int(line[3]),
+    #                 release_date=line[4],
+    #                 lte_exists=line[5],
+    #                 slug=slugify(line[1]),)
